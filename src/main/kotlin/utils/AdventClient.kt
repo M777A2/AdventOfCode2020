ef452985a5aca3b4dba4ff5df3f06f73e7be355c
Zephan The <zthe@adobe.com>
2020-12-08 01:32:04 +0800

    Give up on directly posting answers

--- src/main/kotlin/utils/AdventClient.kt
+++ src/main/kotlin/utils/AdventClient.kt
@@ -1,31 +1,44 @@
 package utils
 
 import io.ktor.client.*
-import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
+import java.lang.Exception
 
 class AdventClient(private val session: String) {
-    private val client = HttpClient(CIO)
+    private val client = HttpClient() {
+        install(HttpRedirect) {
+            checkHttpMethod = false
+        }
+    }
 
     suspend fun getInputForDay(day: Int): String {
         println("getting input for day $day")
         if (day < 1 || day > 25){
             throw Error("Day out of Range")
         }
-        return client.get("https://adventofcode.com/2020/day/$day/input") {
-            header("cookie", "session=$session")
+        return try {
+            client.get("https://adventofcode.com/2020/day/$day/input") {
+                header("cookie", "session=$session")
+            }
+        }
+        catch (e: Exception) {
+            println(e.message)
+            ""
         }
     }
 
     suspend fun postAnswerForDay(day: Int, level: Int, answer: String): String {
-        return client.post("https://adventofcode.com/2020/day/$day/answer") {
-            header("cookie", "session=$session")
-            formData {
-                append("answer", answer)
-                append("level", level)
-            }
-        }
+        println("day $day, level $level answer is: $answer")
+        return ""
+//        return client.post("https://adventofcode.com/2020/day/$day/answer") {
+//            header("cookie", "session=$session")
+//            formData {
+//                append("answer", answer)
+//                append("level", level)
+//            }
+//        }
     }
 
 }
