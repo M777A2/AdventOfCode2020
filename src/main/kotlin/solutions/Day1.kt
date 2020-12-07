0133b446d53e0d9005b82590527e3784a91475b4
Zephan The <zthe@adobe.com>
2020-12-07 21:39:14 +0800

    day 1 level 2

--- src/main/kotlin/solutions/Day1.kt
+++ src/main/kotlin/solutions/Day1.kt
@@ -1,10 +1,10 @@
 package solutions
 
-class Day1(input: String): solution {
+class Day1(input: String): Solution() {
     private val entries = input.trim().split("\n").map { it.toInt() }
 
-    override fun calculate(): Any {
-        println("calculating")
+    override fun calculateLevel1(): String {
+        println("calculating level 1")
         //sort entries
         val sorted = entries.sorted().toMutableList()
         //binary search for 2020 match
@@ -16,7 +16,38 @@ class Day1(input: String): solution {
             }
             else{
                 //multiply
-                return sorted[0] * sorted[index]
+                return (sorted[0] * sorted[index]).toString()
+            }
+        }
+        throw Error("No Match Found!")
+    }
+
+    override fun calculateLevel2(): String {
+        println("calculating level 2")
+        //sort entries
+        val sorted = entries.sorted().toMutableList()
+        //binary search for 2020 match
+        while (sorted.isNotEmpty()){
+            //grab first value
+            val first = sorted.removeFirst()
+            //create new list from remaining
+            val nested = sorted.toMutableList()
+            //find two two numbers that fit the remainder with bsearch
+            val target = 2020-first
+            while (nested.isNotEmpty()){
+                val index = nested.binarySearch { it + nested.first() - target }
+                if (index < 0) {
+                    //remove entry if no match found
+                    nested.removeFirst()
+                    //since it will be at least as large as this, no reason to keep checking
+                    if (nested.first()*2 > target){
+                        break
+                    }
+                }
+                else {
+                    //multiply
+                    return (first * nested[0] * nested[index]).toString()
+                }
             }
         }
         throw Error("No Match Found!")
