4d04cf878222b5a75039a8b74862a23b0fa1c09f
Zephan The <zthe@adobe.com>
2020-12-08 01:32:49 +0800

    use switch to select solution, get session from ENV

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,11 +4,18 @@ import utils.AdventClient
 suspend fun main(args: Array<String>) {
     val day: Int = args[0].toInt()
     val level: Int = args[1].toInt()
-    val session: String = args[2]
+    val session: String = System.getenv("session")
 
     val adventClient = AdventClient(session)
     // TODO: 12/7/20 Figure out how to dynamically assign the solver's class (switch statement? Reflection?)
-    val solver = Day1(adventClient.getInputForDay(day))
+    val input = adventClient.getInputForDay(day)
+    val solver = when (day) {
+        1 -> Day1(input)
+        2 -> Day2(input)
+        else -> {
+            throw NotImplementedError()
+        }
+    }
 
     val result = adventClient.postAnswerForDay(day, level, solver.calculate(level))
     println(result)
